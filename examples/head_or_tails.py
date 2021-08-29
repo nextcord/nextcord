f95cf10cff4db7916ff980ebbdabe6275af2d124
Luca <50520164+lucaytpols@users.noreply.github.com>
2021-08-29 19:17:07 +0200

    Update head_or_tails.py

--- examples/head_or_tails.py
+++ examples/head_or_tails.py
@@ -6,18 +6,16 @@ bot = commands.Bot(command_prefix="h!")
 
 @bot.command()
 async def headortails(ctx, answer=None):
-    number = random.randint((0,1))
-    
+    number = random.randint(0,1)
+
     tails = 0
     heads = 1
-    
+
     if answer is None:
         await ctx.send("You have not set an argumeny, please do: h!headsortails `heads` or h!headsortails `tails`")
     else:
-        if answer is not "heads" or answer is not "tails":
-            await ctx.send("Invalid agrument, the arguments are: `heads` or `tails`! ")
-        else:
-            if number == tails :
+        if answer == "heads" or answer == "tails":
+            if number == tails:
                 if answer == "tails":
                     await ctx.send("You won, congratulations!!")
                 else:
@@ -27,5 +25,7 @@ async def headortails(ctx, answer=None):
                     await ctx.send("You won, congratulations!!")
                 else:
                     await ctx.send("Awhhh, you lost! :(")
+        else: 
+            await ctx.send("Invalid agrument, the arguments are: `heads` or `tails`! ")
 
-    
+bot.run("token")
