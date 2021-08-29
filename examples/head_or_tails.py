a31668c8c0a5afe450f83f1e9195ad14a4c8a819
Luca <50520164+lucaytpols@users.noreply.github.com>
2021-08-29 19:38:31 +0200

    Update head_or_tails.py

--- examples/head_or_tails.py
+++ examples/head_or_tails.py
@@ -6,9 +6,8 @@ bot = commands.Bot(command_prefix="$")
 
 @bot.command()
 async def headortails(ctx, answer=None):
-    machine_choice = random.choice(["heads", "tails"])
 
-    if machine_choice == answer:
+    if random.choice(["heads", "tails"]) == answer:
         await ctx.send("Congratulations")
     else:
         await ctx.send("Sorry you lost")
