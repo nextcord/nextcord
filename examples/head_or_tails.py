73a2e5484dc848bcdacf0a0dd6a579aaf2e712bd
quiktea <wishymovies@gmail.com>
2021-08-29 19:05:40 +0100

    update head_or_tails.py example with detailed comments

--- examples/head_or_tails.py
+++ examples/head_or_tails.py
@@ -3,17 +3,18 @@
 
 # example: $headortails heads
 
-import random
+import random # import the standard random library
 
-from nextcord.ext import commands
+from nextcord.ext import commands # import the commands extension from the next cord library
 
 bot = commands.Bot(command_prefix="$")
 
-@bot.command()
+@bot.command() # register a command called 'headortails' using the @bot.command decorator.
 async def headortails(ctx, answer=None):
-    if random.choice(["heads", "tails"]) == answer:
-        await ctx.reply("Congratulations")
+    if random.choice(["heads", "tails"]) == answer: # choose a random string, either 'heads' or 'tails'
+        # from the list and check it against the answer variable
+        await ctx.reply("Congratulations") # reply with 'Congratulations' with a discord reply.
     else:
-        await ctx.reply("Sorry you lost")
+        await ctx.reply("Sorry you lost") # reply with 'Sorry you lost' with a discord reply.
  
 bot.run("token")
