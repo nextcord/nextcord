6b2254c2e9beef48303213903fe72a6637b1c7f9
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2021-08-29 02:04:49 +0200

    - Add example for secure token usage

---
+++ examples/secure_token_storage.py
@@ -0,0 +1,13 @@
+# Your bot token should not be made public with your code. Otherwise anyone can sign in as your bot and mess with its servers.
+# You should load the token as an environment variable, then access it like this:
+
+import os
+
+# There are other ways to load environment variables but this is one
+from dotenv import load_dotenv # https://pypi.org/project/python-dotenv/
+
+load_dotenv()
+token = os.getenv('BOT_TOKEN')
+bot.run(token)
+
+# load_dotenv reads from a file called .env in the same directory as the python files which should containing TOKEN="1234567890"
