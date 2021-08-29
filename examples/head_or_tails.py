e844bba2ced7d254c87bba97e2b39006bbab64dc
Luca <50520164+lucaytpols@users.noreply.github.com>
2021-08-29 19:51:01 +0200

    Update head_or_tails.py

--- examples/head_or_tails.py
+++ examples/head_or_tails.py
@@ -1,3 +1,8 @@
+# This is a example for head and tails
+# It is like coinflip but then in discord.
+
+# example: $headortails heads
+
 import random
 
 from nextcord.ext import commands
@@ -6,7 +11,6 @@ bot = commands.Bot(command_prefix="$")
 
 @bot.command()
 async def headortails(ctx, answer=None):
-
     if random.choice(["heads", "tails"]) == answer:
         await ctx.reply("Congratulations")
     else:
