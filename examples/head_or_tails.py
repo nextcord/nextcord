c9b8b884e55dd9110d642615db6ae1255b143dad
Luca <50520164+lucaytpols@users.noreply.github.com>
2021-08-29 19:37:58 +0200

    Update head_or_tails.py

--- examples/head_or_tails.py
+++ examples/head_or_tails.py
@@ -5,7 +5,7 @@ from nextcord.ext import commands
 bot = commands.Bot(command_prefix="$")
 
 @bot.command()
-async def headortails(ctx, answer=None:
+async def headortails(ctx, answer=None):
     machine_choice = random.choice(["heads", "tails"])
 
     if machine_choice == answer:
