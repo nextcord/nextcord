9bfad24dd47b9d68b3833fac344dee1cdb9eef2c
Tag-Epic <tagepicuwu@gmail.com>
2021-08-29 14:29:59 +0200

    Feat: Bump version

--- nextcord/__init__.py
+++ nextcord/__init__.py
@@ -10,10 +10,10 @@ A basic wrapper for the Discord API.
 """
 
 __title__ = 'discord'
-__author__ = 'Rapptz'
+__author__ = 'tag-epic & Rapptz'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-present Rapptz'
-__version__ = '2.0.0a'
+__version__ = '2.0.0a1'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
