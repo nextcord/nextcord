41945a0287b53f277dd0035644d102f25664c77f
ooliver1 <oliverwilkes2006@icloud.com>
2021-08-30 14:10:04 +0100

    add __version__ and co to discord/__init__.py

--- discord/__init__.py
+++ discord/__init__.py
@@ -63,3 +63,11 @@ from nextcord.stage_instance import *
 from nextcord.interactions import *
 from nextcord.components import *
 from nextcord.threads import *
+
+__title__ = 'discord'
+__author__ = 'tag-epic & Rapptz'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015-present Rapptz'
+__version__ = '2.0.0a1'
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
