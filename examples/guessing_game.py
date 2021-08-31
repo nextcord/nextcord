0169df2d399e0144f51f5b904e13858cc7803f5f
k1 <89650598+1sttt@users.noreply.github.com>
2021-08-31 20:02:46 +0100

    change guessing_game.py check function name
    why is the check function called is_correct? it's not actually checking if the user is correct. changed the name to just check

--- examples/guessing_game.py
+++ examples/guessing_game.py
@@ -6,17 +6,18 @@ from nextcord.ext import commands
 
 bot = commands.Bot(command_prefix='$')
 
+
 @bot.command()
 async def guess(ctx):
     await ctx.send('Guess a number between 1 and 10.')
 
-    def is_correct(m):
+    def check(m):
         return m.author == ctx.author and m.content.isdigit()
 
     answer = random.randint(1, 10)
 
     try:
-        guess = await bot.wait_for('message', check=is_correct, timeout=5.0)
+        guess = await bot.wait_for('message', check=check, timeout=5.0)
     except asyncio.TimeoutError:
         return await ctx.send(f'Sorry, you took too long it was {answer}.')
 
