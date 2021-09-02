67ab3c4533386f68b382c932c4862027b146c6b2
Tag-Epic <tagepicuwu@gmail.com>
2021-09-02 21:20:50 +0200

    Fix: Reviews

--- nextcord/health_check.py
+++ nextcord/health_check.py
@@ -1,12 +1,17 @@
-from pkg_resources import get_distribution
+from pkg_resources import get_distribution, DistributionNotFound
 from warnings import warn
+
 __all__ = ("incompatible_libraries", )
+
 incompatible_libraries = ["discord.py", "discord", "pyfork", "enhanced-dpy"]
 
 for library in incompatible_libraries:
     try:
         get_distribution(library)
         # Library is installed. Throw a warning
-        warn(f"{library} is installed which is incompatible with nextcord. We strongly encourage you to remove this. This can be removed by using `pip3 uninstall {library}`")
-    except:
+        warn(
+                f"{library} is installed which is incompatible with nextcord."
+                f"We strongly encourage you to remove this. This can be removed by using `pip3 uninstall {library}`"
+            )
+    except DistributionNotFound:
         pass
