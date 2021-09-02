fd9e21599e24a05245fe42c9744ea1034b296802
Tag-Epic <tagepicuwu@gmail.com>
2021-09-02 21:39:47 +0200

    Fix: Use a custom warning class

--- nextcord/health_check.py
+++ nextcord/health_check.py
@@ -3,15 +3,20 @@ from warnings import warn
 
 __all__ = ("incompatible_libraries", )
 
+class DistributionWarning(RuntimeWarning):
+    ...
+
 incompatible_libraries = ["discord.py", "discord", "pyfork", "enhanced-dpy"]
 
 for library in incompatible_libraries:
     try:
         get_distribution(library)
         # Library is installed. Throw a warning
-        warn(
-                f"{library} is installed which is incompatible with nextcord."
-                f"We strongly encourage you to remove this. This can be removed by using `pip3 uninstall {library}`"
-            )
+        message = (
+                f"{library} is installed which is incompatible with nextcord. "
+                f"Please remove this library by using `pip3 uninstall {library}`"
+        )
+
+        warn(message, DistributionWarning, stacklevel=0)
     except DistributionNotFound:
         pass
