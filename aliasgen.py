8e1272171c745e2b5cc8f8ed151ee14ee3236bf4
veni-vidi-code <71394987+veni-vidi-code@users.noreply.github.com>
2021-09-07 19:45:24 +0200

    Update aliasgen.py

--- aliasgen.py
+++ aliasgen.py
@@ -1,56 +1,59 @@
-from pathlib import Path
 from importlib import import_module
-from time import sleep
-
-with open(".aliasignorerc") as f:
-    ignores = [line.strip() for line in f.readlines()]
-with open("alias_license.py") as f:
-    license_text = f.read()
-
-print(f"Ignoring: {ignores}")
-
-target_folder = Path("nextcord")
-alias_folder = Path("discord")
+from pathlib import Path
 
 
-def scan_dir(folder):
-    for file in folder.glob("*"):
-        unprefixed = str(file)[len("nextcord") + 1:]
-        if unprefixed in ignores:
-            continue
-        if file.is_file():
-            if not unprefixed.endswith(".py"):
+def createalias() -> [str]:
+    with open(".aliasignorerc") as f:
+        ignores = [line.strip() for line in f.readlines()]
+    with open("alias_license.py") as f:
+        license_text = f.read()
+
+    target_folder = Path("nextcord")
+    alias_folder = Path("discord")
+
+    def scan_dir(folder):
+        for file in folder.glob("*"):
+            unprefixed = str(file)[len("nextcord") + 1:]
+            if unprefixed in ignores:
                 continue
-            # Should create a alias
-            import_name = str(file)[:-3].replace("/", ".").replace("\\", ".")
-            module = import_module(import_name)
-            alias_file = ""
-
-            # License text
-            alias_file += license_text
-
-            # Imports
-            module_attrs = dir(module)
-            attrs = []
-            for attr in module_attrs:
-                if attr.startswith("__"):
+            if file.is_file():
+                if not unprefixed.endswith(".py"):
                     continue
-                attrs.append(attr)
-            if len(attrs) > 0:
-                alias_file += f"from {import_name} import {', '.join(attrs)}\n"
+                # Should create a alias
+                import_name = str(file)[:-3].replace("/", ".").replace("\\", ".")
+                module = import_module(import_name)
+                alias_file = ""
 
-            # __all__
-            module_all = getattr(module, "__all__", None)
-            if module_all is not None:
-                module_all_quotes = ", ".join([f"\"{item}\"" for item in module_all])
-                module_all_out = f"__all__ = ({module_all_quotes})"
+                # License text
+                alias_file += license_text
 
-                alias_file += module_all_out
+                # Imports
+                module_attrs = dir(module)
+                attrs = []
+                for attr in module_attrs:
+                    if attr.startswith("__"):
+                        continue
+                    attrs.append(attr)
+                if len(attrs) > 0:
+                    alias_file += f"from {import_name} import {', '.join(attrs)}\n"
 
-            with (alias_folder / unprefixed).open("w+") as f:
-                f.write(alias_file)
-        else:
-            scan_dir(folder / unprefixed)
+                # __all__
+                module_all = getattr(module, "__all__", None)
+                if module_all is not None:
+                    module_all_quotes = ", ".join([f"\"{item}\"" for item in module_all])
+                    module_all_out = f"__all__ = ({module_all_quotes})"
+
+                    alias_file += module_all_out
+
+                with (alias_folder / unprefixed).open("w+") as f:
+                    f.write(alias_file)
+            else:
+                scan_dir(folder / unprefixed)
+
+    scan_dir(target_folder)
+    return ignores
 
 
-scan_dir(target_folder)
+if __name__ == '__main__':
+    print("Creating aliases")
+    print(f"Finished. Ignoring {', '.join(createalias())}")
