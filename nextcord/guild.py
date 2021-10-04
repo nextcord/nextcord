f25ea0d26a431f1dff5c0ec1188b79442539d079
ooliver1 <oliverwilkes2006@icloud.com>
2021-10-04 21:31:31 +0100

    Add student hub type and doc the feature

--- nextcord/guild.py
+++ nextcord/guild.py
@@ -203,6 +203,7 @@ class Guild(Hashable):
         - ``COMMUNITY``: Guild is a community server.
         - ``DISCOVERABLE``: Guild shows up in Server Discovery.
         - ``FEATURABLE``: Guild is able to be featured in Server Discovery.
+        - ``HUB``: Guild is a student hub.
         - ``INVITE_SPLASH``: Guild's invite page can have a special splash.
         - ``MEMBER_VERIFICATION_GATE_ENABLED``: Guild has Membership Screening enabled.
         - ``MONETIZATION_ENABLED``: Guild has enabled monetization.
