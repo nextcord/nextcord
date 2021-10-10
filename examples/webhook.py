55baf47c40fc48d4f6b610846d3d8a117c1a1b09
Suven Pandey <pandeysuven@gmail.com>
2021-10-10 22:05:54 +0545

    Remove sync example

--- examples/webhook.py
+++ examples/webhook.py
@@ -6,9 +6,3 @@ async def webhook_url(url, content):
     async with aiohttp.ClientSession() as session:
         webhook = nextcord.Webhook.from_url(url, session=session)
         await webhook.send(content)
-
-# Synchronous version of webhook_url
-def webhook_url_sync(url, content):
-    with requests.Session() as session:
-        webhook = nextcord.SyncWebhook.from_url(url, session=session)
-        webhook.send(content)
