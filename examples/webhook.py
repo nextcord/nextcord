8086ffb7a61903e3e900329de6642e3ced8d111d
Suven Pandey <pandeysuven@gmail.com>
2021-10-25 16:47:51 +0545

    Run function

--- examples/webhook.py
+++ examples/webhook.py
@@ -1,7 +1,13 @@
 import aiohttp
+import asyncio
 import nextcord
 
-async def webhook_url(url, content):
+async def send_to_webhook(url, content):
     async with aiohttp.ClientSession() as session:
         webhook = nextcord.Webhook.from_url(url, session=session)
         await webhook.send(content)
+
+loop = asyncio.get_event_loop()
+url = 'url'
+content = 'Hello, world!'
+loop.run_until_complete(send_to_webhook(url, content))
