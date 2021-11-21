14cc7ca0bfaae49d8dc4d6a4a45bce5d49a7188d
ooliver1 <oliverwilkes2006@icloud.com>
2021-11-21 14:51:55 +0000

    fix(audit): fix a weird type

--- nextcord/audit_logs.py
+++ nextcord/audit_logs.py
@@ -146,7 +146,7 @@ def _enum_transformer(enum: Type[T]) -> Callable[[AuditLogEntry, int], T]:
 
     return _transform
 
-def _transform_type(entry: AuditLogEntry, data: Union[int]) -> Union[enums.ChannelType, enums.StickerType]:
+def _transform_type(entry: AuditLogEntry, data: int) -> Union[enums.ChannelType, enums.StickerType]:
     if entry.action.name.startswith('sticker_'):
         return enums.try_enum(enums.StickerType, data)
     else:
