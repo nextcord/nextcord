11d894090280843799e7856394af31b9e9744618
Suven-p <pandeysuven@gmail.com>
2021-11-22 10:53:19 +0545

    Add comments

--- examples/webhook.py
+++ examples/webhook.py
@@ -1,8 +1,12 @@
+# This is an example for sending message to a channel using its webhook url.
+# You can use this to send messages to a channel without using the bot.
+
 import aiohttp
 import asyncio
 import nextcord
 
 async def send_to_webhook(url, content):
+    # Create a new HTTP session and use it to create webhook object
     async with aiohttp.ClientSession() as session:
         webhook = nextcord.Webhook.from_url(url, session=session)
         await webhook.send(content)
