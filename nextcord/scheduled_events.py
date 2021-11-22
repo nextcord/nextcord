bd496634d0b1681787788c1ab4d137101f7e4928
ooliver1 <oliverwilkes2006@icloud.com>
2021-11-22 17:03:57 +0000

    refactor(events): change for useability n stuff

--- nextcord/scheduled_events.py
+++ nextcord/scheduled_events.py
@@ -24,7 +24,7 @@ DEALINGS IN THE SOFTWARE.
 
 from __future__ import annotations
 
-from typing import Optional, Tuple, TYPE_CHECKING
+from typing import Any, Optional, Tuple, TYPE_CHECKING
 
 from .enums import PrivacyLevel
 from .errors import InvalidArgument
@@ -78,7 +78,7 @@ class ScheduledEvent(Hashable):
         self._state: ConnectionState = state
         self._from_data(data)
 
-    def _from_data(self, data: ScheduledEventPayload) -> None:
+    def _update(self, data: ScheduledEventPayload) -> None:
         self.id: int = int(data['id'])
         if creator := data.get('creator'):
             self.creator: Optional[User] = self._state.store_user(creator)
@@ -89,10 +89,7 @@ class ScheduledEvent(Hashable):
         self.start_time: datetime = parse_time(data['scheduled_start_time'])
         self.end_time: Optional[datetime] = parse_time(data['scheduled_end_time'])
         self.privacy_level: PrivacyLevel = PrivacyLevel(data['privacy_level'])
-        if (_ := data.get('entity_metadata')) and (location := _.get('location')):
-            self.location: str = location
-        else:
-            self.location: str = ''
+        self.metadata: EntityMetadata = EntityMetadata(**data.get('metadata', {}))
         self.user_count: int = data.get('user_count', 0)
         self.channel: Optional[GuildChannel] = self._state.get_channel(
             int(data.get('channel_id'))
@@ -105,6 +102,10 @@ class ScheduledEvent(Hashable):
     def __repr__(self) -> str:
         value = ' '.join(f'{attr}={getattr(self, attr)!r}' for attr in self.__slots__)
         return f'<{self.__class__.__name__} {value}>'
+
+    @property
+    def location(self) -> str:
+        return self.metadata.location
 
     async def delete(self) -> None:
         await self._state.http.delete_event(self.guild.id, self.id)
