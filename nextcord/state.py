6991ab598db49adc797a8f5cf2c2fa72e8fc8e7a
ooliver1 <oliverwilkes2006@icloud.com>
2021-12-09 18:05:49 +0000

    fix(event): forgor to add to cache on event create

--- nextcord/state.py
+++ nextcord/state.py
@@ -1416,11 +1416,12 @@ class ConnectionState:
     def parse_guild_scheduled_event_create(self, data) -> None:
         if guild := self._get_guild(int(data['guild_id'])):
             event = self.create_scheduled_event(guild=guild, data=data)
+            guild._add_event(event)
             self.dispatch('guild_scheduled_event_create', event)
         else:
             _log.debug('GUILD_SCHEDULED_EVENT_CREATE referencing unknown guild '
                        'ID: %s. Discarding.', data['guild_id'])
-
+int()
     def parse_guild_scheduled_event_update(self, data) -> None:
         if guild := self._get_guild(int(data['guild_id'])):
             if event := guild.get_scheduled_event(int(data['id'])):
