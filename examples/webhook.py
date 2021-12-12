18477ac92d9e3de9e3d565fa5cbb778e0d24aa08
Oliver Wilkes <34910574+ooliver1@users.noreply.github.com>
2021-12-12 10:01:12 +0000

    Update examples/webhook.py
    its quite stale so I'll just add it

--- examples/webhook.py
+++ examples/webhook.py
@@ -11,5 +11,4 @@ async def send_to_webhook(url, content):
         webhook = nextcord.Webhook.from_url(url, session=session)
         await webhook.send(content)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(send_to_webhook('url', 'Hello, world!'))
+asyncio.run(send_to_webhook("url", "Hello, world!"))
