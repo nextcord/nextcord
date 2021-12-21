5fd3d6fb5b5c6c5acdf9cab1caa4c1b995a0ca39
ooliver1 <oliverwilkes2006@icloud.com>
2021-12-21 19:49:29 +0000

    perf(member): update docs

--- nextcord/member.py
+++ nextcord/member.py
@@ -29,19 +29,29 @@ import inspect
 import itertools
 import sys
 from operator import attrgetter
-from typing import Any, Dict, List, Literal, Optional, TYPE_CHECKING, Tuple, Type, TypeVar, Union, overload
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    List,
+    Literal,
+    Optional,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+    overload,
+)
 
-from . import abc
-
-from . import utils
+from . import abc, utils
+from .activity import ActivityTypes, create_activity
 from .asset import Asset
-from .utils import MISSING
-from .user import BaseUser, User, _UserTag
-from .activity import create_activity, ActivityTypes
-from .permissions import Permissions
-from .enums import Status, try_enum
 from .colour import Colour
+from .enums import Status, try_enum
 from .object import Object
+from .permissions import Permissions
+from .user import BaseUser, User, _UserTag
+from .utils import MISSING
 
 __all__ = (
     'VoiceState',
@@ -49,21 +59,19 @@ __all__ = (
 )
 
 if TYPE_CHECKING:
+    from .abc import Snowflake
     from .asset import Asset
-    from .channel import DMChannel, VoiceChannel, StageChannel
+    from .channel import DMChannel, StageChannel, VoiceChannel
     from .flags import PublicUserFlags
     from .guild import Guild
-    from .types.activity import PartialPresenceUpdate
-    from .types.member import (
-        MemberWithUser as MemberWithUserPayload,
-        Member as MemberPayload,
-        UserWithMember as UserWithMemberPayload,
-    )
-    from .types.user import User as UserPayload
-    from .abc import Snowflake
-    from .state import ConnectionState
     from .message import Message
     from .role import Role
+    from .state import ConnectionState
+    from .types.activity import PartialPresenceUpdate
+    from .types.member import Member as MemberPayload
+    from .types.member import MemberWithUser as MemberWithUserPayload
+    from .types.member import UserWithMember as UserWithMemberPayload
+    from .types.user import User as UserPayload
     from .types.voice import VoiceState as VoiceStatePayload
 
     VocalGuildChannel = Union[VoiceChannel, StageChannel]
@@ -254,18 +262,18 @@ class Member(abc.Messageable, _UserTag):
     """
 
     __slots__ = (
+        '_roles',
+        'joined_at',
+        'premium_since',
         'activities',
         'guild',
-        'joined_at',
-        'nick',
         'pending',
-        'premium_since',
-        '_avatar',
+        'nick',
         '_client_status',
-        '_roles',
-        '_state',
-        '_timeout',
         '_user',
+        '_state',
+        '_avatar',
+        '_timeout',
     )
 
     if TYPE_CHECKING:
@@ -617,6 +625,10 @@ class Member(abc.Messageable, _UserTag):
         """Optional[:class:`datetime.datetime`]: A datetime object that represents 
         the time in which the member will be able to interact again.
 
+        .. note::
+
+            This is ``None`` if the user can chat/interact.
+
         .. versionadded:: 2.0
         """
         if self._timeout is None or self._timeout < utils.utcnow():
@@ -713,7 +725,7 @@ class Member(abc.Messageable, _UserTag):
             The reason for editing this member. Shows up on the audit log.
         timeout: Optional[:class:`datetime.datetime`]
             The time until the member should not be timed out.
-            Set this to None to turn off their timeout.
+            Set this to None to disable their timeout.
 
         Raises
         -------
