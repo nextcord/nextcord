cbfbc5d12f6ecf8d04fc73c3d18946f0e5ab5bf3
Vincent <vincentbusiness55@gmail.com>
2021-12-31 08:59:21 +0800

    Fix up options example

--- examples/application_commands/options.py
+++ examples/application_commands/options.py
@@ -7,13 +7,13 @@ client = nextcord.Client()
 
 @client.slash_command(guild_ids=[...])  # Limits the guildes
 async def my_select_command(
-    interaction: Interaction, help: str = SlashOption(name="Say a help option!")
+    interaction: Interaction, help: str = SlashOption(name="option", description="The option you want")
 ):
     if help == "fun":
         interaction.response.send_message("Fun!")
     elif help == "music":
         interaction.response.send_message(
-            "Sorry lavalink support hasn't come to the line!"
+            "Music.."
         )
     elif help == "mod":
         interaction.response.send_message(
@@ -21,6 +21,9 @@ async def my_select_command(
         )
     else:
         interaction.response.send_message("Could not find that help option!")
-
+        
+@client.slash_command(guild_ids=[...]) # limits the guilds with this command
+async def hi(interacton: Interaction, member: nextcord.Member = SlashOption(name="user", description="the user to say hi to")):
+    await interaction.response.send_message(f"{interaction.user} just said hi to {member}”)
 
 client.run("TOKEN")
