57400f9e4823e97ac9538288c06b685d5439d231
Lala Sabathil <aiko@aitsys.dev>
2022-01-17 19:29:48 +0100

    Merge: #298

--- nextcord/interactions.py
+++ nextcord/interactions.py
@@ -91,6 +91,10 @@ class Interaction:
         The guild ID the interaction was sent from.
     channel_id: Optional[:class:`int`]
         The channel ID the interaction was sent from.
+    locale: Optional[:class:`str`]
+        The users locale.
+    guild_locale: Optional[:class:`str`]
+        The guilds preferred locale, if invoked in a guild.
     application_id: :class:`int`
         The application ID that the interaction was for.
     user: Optional[Union[:class:`User`, :class:`Member`]]
@@ -113,6 +117,8 @@ class Interaction:
         'application_id',
         'message',
         'user',
+        'locale',
+        'guild_locale',
         'token',
         'version',
         '_permissions',
@@ -139,6 +145,8 @@ class Interaction:
         self.channel_id: Optional[int] = utils._get_as_snowflake(data, 'channel_id')
         self.guild_id: Optional[int] = utils._get_as_snowflake(data, 'guild_id')
         self.application_id: int = int(data['application_id'])
+        self.locale: Optional[str] = data.get('locale')
+        self.guild_locale: Optional[str] = data.get('guild_locale')
 
         self.message: Optional[Message]
         try:
