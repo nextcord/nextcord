ef37c2ca986fa3affb9fc835b45fe2167a2abb54
Vincent <vincentbusiness55@gmail.com>
2022-01-23 07:44:51 +0800

    chore: apply suggestions

--- examples/application_commands/cog_example.py
+++ examples/application_commands/cog_example.py
@@ -1,11 +1,4 @@
-from nextcord import (
-    Interaction,
-    message_command,
-    slash_command,
-    user_command,
-    Message,
-    Member,
-)
+import nextcord
 from nextcord.ext import commands
 
 
@@ -13,17 +6,17 @@ class ApplicationCommandCog(commands.Cog):  # Subclassing commands.Cog as you al
     def __init__(self, bot: commands.Bot):  # takes in bot for the setup later
         self.bot = bot  # making it global to this class.
 
-    @slash_command(guild_ids=[...])  # isn't really any different to normal commands.
-    async def my_slash_command(self, interaction: Interaction):
+    @nextcord.slash_command(guild_ids=[...])  # isn't really any different to normal commands.
+    async def my_slash_command(self, interaction: nextcord.Interaction):
         await interaction.response.send_message("This is a slash command in a cog!")
 
-    @user_command(guild_ids=[...])  # Again very simular to normal commands.
-    async def my_user_command(self, interaction: Interaction, member: Member):  # passing in member as it is intaked
+    @nextcord.user_command(guild_ids=[...])  # Again very simular to normal commands.
+    async def my_user_command(self, interaction: nextcord.Interaction, member: nextcord.Member):  # passing in member as it is intaked
         await interaction.response.send_message(f"Hello, {member}!")
 
-    @message_command(guild_ids=[...])  # limits guilds
+    @nextcord.message_command(guild_ids=[...])  # limits guilds
     async def my_message_command(
-        self, interaction: Interaction, message: Message
+        self, interaction: nextcord.Interaction, message: nextcord.Message
     ):  # passing in message as it is intaked
         await interaction.response.send_message(f"{message}")
 
