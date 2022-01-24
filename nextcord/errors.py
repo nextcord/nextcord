68058cb0607106653edb1d5a6aedc69ce1413682
okayyyy <zghan2005@gmail.com>
2022-01-23 17:00:24 -0800

    remove duplicate ApplicationError

--- nextcord/errors.py
+++ nextcord/errors.py
@@ -302,23 +302,6 @@ class ApplicationError(DiscordException):
         else:
             super().__init__(*args)
 
-class ApplicationError(DiscordException):
-    r"""The base exception type for all application command related errors.
-
-    This inherits from :exc:`nextcord.DiscordException`.
-
-    This exception and exceptions inherited from it are handled
-    in a special way as they are caught and passed into a special event
-    from :class:`.Bot`\, :func:`.on_application_error`.
-    """
-    def __init__(self, message: Optional[str] = None, *args: Any) -> None:
-        if message is not None:
-            # clean-up @everyone and @here mentions
-            m = message.replace('@everyone', '@\u200beveryone').replace('@here', '@\u200bhere')
-            super().__init__(m, *args)
-        else:
-            super().__init__(*args)
-
 class ApplicationInvokeError(ApplicationError):
     """Exception raised when the command being invoked raised an exception.
 
