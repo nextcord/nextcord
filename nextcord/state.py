5fc26c55e7f31a7dbc45c94a86d98cbc011c45fe
Gugu7264 <github@gurvan.fr>
2022-01-24 20:32:40 +0100

    fix: Guild.chunk ignoring cache parameter

--- nextcord/state.py
+++ nextcord/state.py
@@ -1370,7 +1370,8 @@ class ConnectionState:
         return guild.id not in self._guilds
 
     async def chunk_guild(self, guild, *, wait=True, cache=None):
-        cache = cache or self.member_cache_flags.joined
+        if cache is None:
+            cache = self.member_cache_flags.joined
         request = self._chunk_requests.get(guild.id)
         if request is None:
             self._chunk_requests[guild.id] = request = ChunkRequest(guild.id, self.loop, self._get_guild, cache=cache)
