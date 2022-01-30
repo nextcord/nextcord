8db3c483560b48ecab1b079ff17feda1f227146a
Jonah Lawrence <jonah@freshidea.com>
2022-01-30 16:28:40 -0700

    docs(examples): fix imports

--- examples/application_commands/choices.py
+++ examples/application_commands/choices.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import nextcord
 from nextcord import Interaction, SlashOption
 
--- examples/application_commands/slash_command.py
+++ examples/application_commands/slash_command.py
@@ -1,5 +1,5 @@
-from typing import Optional
 import nextcord
+from nextcord import Interaction, SlashOption
 
 TESTING_GUILD_ID = 123456789  # Replace with your testing guild id
 
@@ -7,17 +7,17 @@ client = nextcord.Client()
 
 # command will be global if guild_ids is not specified
 @client.slash_command(guild_ids=[TESTING_GUILD_ID])
-async def ping(interaction: nextcord.Interaction):
+async def ping(interaction: Interaction):
     await interaction.response.send_message("Pong!")
 
 
 @client.slash_command(guild_ids=[TESTING_GUILD_ID])
-async def echo(interaction: nextcord.Interaction, arg: str):
+async def echo(interaction: Interaction, arg: str):
     await interaction.response.send_message(arg)
 
 
 @client.slash_command(guild_ids=[TESTING_GUILD_ID])
-async def enter_a_number(interaction: nextcord.Interaction, number: int = SlashOption(required=False)):
+async def enter_a_number(interaction: Interaction, number: int = SlashOption(required=False)):
     if not number:
         await interaction.response.send_message("You need to specify a number!", ephemeral=True)
     else:
