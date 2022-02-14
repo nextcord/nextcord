dc07ee547f6fa411151b098a49e662feda4d2fd4
Jonah Lawrence <jonah@freshidea.com>
2022-02-14 00:25:30 -0700

    Adds descriptions to slash command examples (#454)
    * docs(assets): renamed assets
    
    * docs(assets): added halloween repo banner
    
    * docs(assets): fix naming again
    
    * docs(readme): fix naming in readme
    
    * docs(assets): Removed halloween repo banner
    
    https://github.com/nextcord/nextcord/pull/202#issuecomment-936031646
    
    * docs(assets): renamed assets
    
    * docs(assets): added halloween repo banner
    
    * docs(assets): fix naming again
    
    * docs(assets): Removed halloween repo banner
    
    https://github.com/nextcord/nextcord/pull/202#issuecomment-936031646
    
    * docs(examples): Add descriptions to slash commands
    
    * docs(examples): Add description to slash command

--- examples/application_commands/slash_command.py
+++ examples/application_commands/slash_command.py
@@ -6,20 +6,23 @@ TESTING_GUILD_ID = 123456789  # Replace with your testing guild id
 client = nextcord.Client()
 
 # command will be global if guild_ids is not specified
-@client.slash_command(guild_ids=[TESTING_GUILD_ID])
+@client.slash_command(guild_ids=[TESTING_GUILD_ID], description="Ping command")
 async def ping(interaction: Interaction):
     await interaction.response.send_message("Pong!")
 
 
-@client.slash_command(guild_ids=[TESTING_GUILD_ID])
-async def echo(interaction: Interaction, arg: str):
+@client.slash_command(guild_ids=[TESTING_GUILD_ID], description="Repeats your message")
+async def echo(interaction: Interaction, arg: str = SlashOption(description="Message")):
     await interaction.response.send_message(arg)
 
 
-@client.slash_command(guild_ids=[TESTING_GUILD_ID])
-async def enter_a_number(interaction: Interaction, number: int = SlashOption(required=False)):
+@client.slash_command(guild_ids=[TESTING_GUILD_ID], description="Choose a number")
+async def enter_a_number(
+    interaction: Interaction,
+    number: int = SlashOption(description="Your number", required=False),
+):
     if not number:
-        await interaction.response.send_message("You need to specify a number!", ephemeral=True)
+        await interaction.response.send_message("No number was specified!", ephemeral=True)
     else:
         await interaction.response.send_message(f"You chose {number}!")
 
