42cbbd792e720b6b6c837a319eed00676e7bbf29
Epic <tagepicuwu@gmail.com>
2022-02-15 17:37:23 +0100

    Update __init__.py

--- discord/__init__.py
+++ discord/__init__.py
@@ -25,7 +25,7 @@ from nextcord import *
 __title__ = 'nextcord'
 __author__ = 'tag-epic & Rapptz'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2015-present Rapptz'
+__copyright__ = 'Copyright 2015-present Rapptz & tag-epic'
 __version__ = '2.0.0a3'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
