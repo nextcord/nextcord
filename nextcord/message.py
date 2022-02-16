12e0815d23e05cc2b5c3e7dcd4e7251b8034978b
Minh Dương <i-am@maskduck.ninja>
2022-02-16 14:04:42 +0700

    fix formatting *again*

--- nextcord/message.py
+++ nextcord/message.py
@@ -1235,6 +1235,7 @@ class Message(Hashable):
         attachments: List[:class:`Attachment`]
             A list of attachments to keep in the message. If ``[]`` is passed
             then all attachments are removed.
+            
             .. note::
                 Due to recent API change, existing attachments must be specified when editing messages with new attachments. Any attachments not specified will be removed and replaced with the specified list.
         suppress: :class:`bool`
