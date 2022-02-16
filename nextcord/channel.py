6420e71905095be616187c4f44122fa6490fc58d
Minh Dương <i-am@maskduck.ninja>
2022-02-16 18:31:04 +0700

    actually these ws are actually important

--- nextcord/channel.py
+++ nextcord/channel.py
@@ -715,7 +715,7 @@ class TextChannel(abc.Messageable, abc.GuildChannel, Hashable):
             Starting the thread failed.
 
             .. note:: 
-
+                
                 This can include if the guild isn't at least level two boosting yet and you are trying to create a private thread.
 
         Returns
