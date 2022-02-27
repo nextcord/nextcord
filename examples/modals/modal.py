6c38b0132ba801b0278185a7b3054d9b87c67139
ascpial <mail@ascpial.fr>
2022-02-27 22:07:00 +0100

    feat: added examples

---
+++ examples/modals/modal.py
@@ -0,0 +1,42 @@
+import nextcord
+
+TESTING_GUILD_ID = 123456798  # Replace with your testing guild id
+
+class Pet(nextcord.ui.Modal):
+    def __init__(self):
+        super().__init__(
+            "Your pet",
+            timeout=5*60, # 5 minutes
+        )
+        self.name = nextcord.ui.TextInput(
+            label="Your pet's name",
+            max_length=50,
+        )
+        self.description = nextcord.ui.TextInput(
+            label="Description",
+            style=nextcord.TextInputStyle.paragraph,
+            placeholder="Information that can help us recognise your pet",
+            required=False,
+            max_length=1800,
+        )
+        self.add_item(self.name)
+        self.add_item(self.description)
+    
+    async def callback(self, inter: nextcord.Interaction) -> None:
+        response = f"{inter.user.mention} favourite pet's name is {self.name.value}."
+        if self.description.value != "":
+            response += f"\nIts pet can be recognized by this information:\n{self.description.value}"
+        await inter.send(response)
+
+bot = nextcord.Client()
+
+@bot.slash_command(
+    name="pet",
+    description="Describe your favourite pet",
+    guild_ids=[TESTING_GUILD_ID],
+)
+async def send(inter: nextcord.Interaction):
+    modal = Pet()
+    await inter.response.send_modal(modal)
+
+bot.run("token")
---
+++ examples/modals/persistent.py
@@ -0,0 +1,59 @@
+import nextcord
+
+TESTING_GUILD_ID = 123456798  # Replace with your testing guild id
+
+class Feedback(nextcord.ui.Modal):
+    def __init__(self):
+        super().__init__(
+            title="Feedback",
+            custom_id="feedback",
+            timeout=None,
+        )
+        self.discovered = nextcord.ui.TextInput(
+            label="How did you discover the bot?",
+            required=False,
+            style=nextcord.TextInputStyle.paragraph,
+            custom_id="discovered",
+        )
+        self.rating = nextcord.ui.TextInput(
+            label="How would you rate the bot?",
+            placeholder="I would give the bot a ten out of ten...",
+            custom_id="rating",
+        )
+        self.improve = nextcord.ui.TextInput(
+            label="How could the bot improve?",
+            style=nextcord.TextInputStyle.paragraph,
+            required=False,
+            custom_id="improve",
+        )
+        self.add_item(self.discovered)
+        self.add_item(self.rating)
+        self.add_item(self.improve)
+    
+    async def callback(self, interaction: nextcord.Interaction):
+        await interaction.send(
+            f"{interaction.user.mention} feedback :\n"
+            f"Rating: {self.rating.value}\n"
+            f"Where did he discover the bot: {self.discovered.value}\n"
+            f"How could the bot improve: {self.improve.value}\n"
+        )
+
+bot = nextcord.Client()
+
+feedback_modal = None
+
+@bot.event
+async def on_ready():
+    global feedback_modal
+    feedback_modal = Feedback()
+    bot.add_modal(feedback_modal)
+
+@bot.slash_command(
+    name="feedback",
+    description="Send your feedback to the bot developer!",
+    guild_ids=[TESTING_GUILD_ID],
+)
+async def feedback(inter: nextcord.Interaction):
+    await inter.response.send_modal(feedback_modal)
+
+bot.run("token")
