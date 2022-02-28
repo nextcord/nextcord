022fcdff4522eaa76d390e02a5795d03dc5413e1
MaskDuck <i-am@maskduck.ninja>
2022-02-28 19:09:48 +0700

    add docs for icon

--- nextcord/role.py
+++ nextcord/role.py
@@ -403,6 +403,8 @@ class Role(Hashable):
         position: :class:`int`
             The new role's position. This must be below your top role's
             position or it will fail.
+        icon: :class:`File`
+            The role's icon image, if the guild has the ROLE_ICONS feature.
         reason: Optional[:class:`str`]
             The reason for editing this role. Shows up on the audit log.
 
