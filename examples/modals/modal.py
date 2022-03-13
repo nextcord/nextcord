d501ca8245b2b0546f5834d74208083960bc0e6f
Jonah Lawrence <jonah@freshidea.com>
2022-03-13 14:29:02 -0600

    Remove parentheses in example

--- examples/modals/modal.py
+++ examples/modals/modal.py
@@ -27,7 +27,7 @@ class Pet(nextcord.ui.Modal):
         self.add_item(self.description)
 
     async def callback(self, interaction: nextcord.Interaction) -> None:
-        response = (f"{interaction.user.mention} favourite pet's name is {self.name.value}.")
+        response = f"{interaction.user.mention} favourite pet's name is {self.name.value}."
         if self.description.value != "":
             response += f"\nTheir pet can be recognized by this information:\n{self.description.value}"
         await interaction.send(response)
