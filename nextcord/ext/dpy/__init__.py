b9d07aa42bad1fecfde3126ad1b4270d53e537ab
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2022-04-12 22:34:42 -0500

    The license was very much the wrong year, whoops.
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- nextcord/ext/dpy/__init__.py
+++ nextcord/ext/dpy/__init__.py
@@ -2,7 +2,7 @@
 nextcord.ext.dpy
 ~~~~~~~~~~~~~~~~~~~~~
 An extension module to replicate parts of Discord.py.
-:copyright: (c) 2021-present tag-epic
+:copyright: (c) 2022-present tag-epic
 :license: MIT, see LICENSE for more details.
 """
 
