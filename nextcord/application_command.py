f0d25fb2ae0e9a22eedb3de7fb280e4985a0658b
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2022-06-14 09:39:42 -0500

    At least you tell me what to do this time.
    Signed-off-by: Alex Schoenhofen <alexanderschoenhofen@gmail.com>

--- nextcord/application_command.py
+++ nextcord/application_command.py
@@ -1329,7 +1329,7 @@ class SlashCommandOption(BaseCommandOption, SlashOption, AutocompleteOptionMixin
             # grouped_annotations: List[Union[type, Annotated[object, OptionConverter]]] = unpacked_annotations + \
             #                                                                              literals
             grouped_annotations: List[
-                Union[type, Annotated[Optional[OptionConverter], object], type(None)]
+                Union[type, Annotated[Optional[OptionConverter], object], Type[None]]
             ] = []
             grouped_annotations.extend(unpacked_annotations)
             grouped_annotations.extend(literals)
