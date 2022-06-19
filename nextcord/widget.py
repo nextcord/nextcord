7ee3f21022c505d27cd11d4c4183ba242e920262
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-06-20 04:27:43 +0700

    chore: add tag-epic to license headers (#617)
    * fix(raw-models): add tag-epic to license headers
    
    * Update threads.py
    
    * Update threads.py
    
    * Update widget.py
    
    * Update components.py
    
    * Update channel.py
    
    * style: auto fixes from pre-commit hooks
    
    Co-authored-by: ooliver1 <oliverwilkes2006@icloud.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- nextcord/widget.py
+++ nextcord/widget.py
@@ -1,7 +1,8 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2015-present Rapptz
+Copyright (c) 2015-2021 Rapptz
+Copyright (c) 2022-present tag-epic
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
