2a7f8418b5485195cf0d80bb0de406bd25a41342
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-06-20 16:37:04 +0700

    Update nextcord/automod.py
    Co-authored-by: Oliver Wilkes <oliverwilkes2006@icloud.com>

--- nextcord/automod.py
+++ nextcord/automod.py
@@ -59,7 +59,8 @@ class AutoModerationAction:
     notify_channel_id: Optional[:class:`int`]
         The ID of the channel that this action will notify in.
     timeout_seconds: Optional[:class:`int`]
-        The number of seconds this rule should timeout when someone breaks."""
+        The number of seconds this rule should timeout when someone breaks.
+    """
 
     def __init__(self, data: AutoModerationActionPayload):
         self.type: ActionType = try_enum(ActionType, data["type"])
