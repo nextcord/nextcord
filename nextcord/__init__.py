3eeba694b2b4bfd464c290342030f7d040e8ff9d
ooliver1 <oliverwilkes2006@icloud.com>
2022-06-23 12:18:18 +0100

    chore: bump version

--- nextcord/__init__.py
+++ nextcord/__init__.py
@@ -14,7 +14,7 @@ __title__ = "nextcord"
 __author__ = "tag-epic & Rapptz"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015-present Rapptz & tag-epic"
-__version__ = "2.0.0rc1"
+__version__ = "2.0.0rc2"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
@@ -75,7 +75,7 @@ class VersionInfo(NamedTuple):
 
 
 version_info: VersionInfo = VersionInfo(
-    major=2, minor=0, micro=0, releaselevel="candidate", serial=1
+    major=2, minor=0, micro=0, releaselevel="candidate", serial=2
 )
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
