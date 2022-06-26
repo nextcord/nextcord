2b26bead1ba754e11df39f4fd7d1058d8cbb25e9
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-06-26 16:05:05 +0700

    Update nextcord/application_command.py

--- nextcord/application_command.py
+++ nextcord/application_command.py
@@ -67,7 +67,7 @@ from .user import User
 from .utils import MISSING, find, maybe_coroutine, parse_docstring
 
 if TYPE_CHECKING:
-    from abc import MessageableChannel
+    from .abc import MessageableChannel
 
     from .state import ConnectionState
     from .types.checks import ApplicationCheck, ApplicationErrorCallback, ApplicationHook
