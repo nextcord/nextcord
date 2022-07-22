0bf6f6b61ac9fe8b96b2b7087b852feceb40c850
MaskDuck <it-is@too-obvious-that.maskduck.is-a.dev>
2022-07-22 07:02:55 +0000

    fix: add none and actually remove view

--- nextcord/ui/view.py
+++ nextcord/ui/view.py
@@ -537,7 +537,7 @@ class ViewStore:
     def remove_view(self, view: View):
         for item in view.children:
             if item.is_dispatchable():
-                self._views.pop((item.type.value, item.custom_id), None)  # type: ignore
+                self._views.pop((item.type.value, None, item.custom_id), None)  # type: ignore
 
         for key, value in self._synced_message_views.items():
             if value.id == view.id:
