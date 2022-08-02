2d11a08e2be52eccfddad4e8e9fede38f3b7372e
TrapDrap <69605288+TrapDrap@users.noreply.github.com>
2022-08-02 19:26:28 -0400

    file.py docs reflect new changes p1
    Co-authored-by: Jonah Lawrence <jonah@freshidea.com>

--- nextcord/file.py
+++ nextcord/file.py
@@ -66,7 +66,7 @@ class File:
     force_close: :class:`bool`
         Whether to forcibly close the bytes used to create the file
         when ``.close()`` is called.
-        This will also make the file bytes unusable by flusing it from
+        This will also make the file bytes unusable by flushing it from
         memory after it is sent once.
         Keep this enabled if you don't wish to reuse the same bytes.
 
