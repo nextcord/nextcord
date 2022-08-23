37ab8a2aa9ef1c54e0ed6bebc7b65fce02774257
Jonah Lawrence <jonah@freshidea.com>
2022-08-23 13:50:27 -0600

    fix: remove deprecated nickname mention syntax

--- nextcord/member.py
+++ nextcord/member.py
@@ -509,8 +509,6 @@ class Member(abc.Messageable, _UserTag):
     @property
     def mention(self) -> str:
         """:class:`str`: Returns a string that allows you to mention the member."""
-        if self.nick:
-            return f"<@!{self._user.id}>"
         return f"<@{self._user.id}>"
 
     @property
