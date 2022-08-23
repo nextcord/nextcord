ca7ad8d886e01437fedcf39bde3c01a64d60c189
Alex Schoenhofen <alexanderschoenhofen@gmail.com>
2022-08-23 18:36:14 -0500

    refactor(app cmds): Use sorted instead of .sort()
    Co-authored-by: Jonah Lawrence <jonah@freshidea.com>

--- nextcord/application_command.py
+++ nextcord/application_command.py
@@ -366,11 +366,9 @@ class ApplicationCommandOption:
 
         if self.channel_types:
             # noinspection PyUnresolvedReferences
-            ret["channel_types"] = [channel_type.value for channel_type in self.channel_types]
-            ret[
-                "channel_types"
-            ].sort()  # When reading application commands from Discord, they return the channel types
-            #  sorted. To prevent needless syncing and allow lazy loading, we need to sort them as well.
+            # When reading application commands from Discord, they return the channel types sorted.
+            # To prevent needless syncing and allow lazy loading, we need to sort them as well.
+            ret["channel_types"] = sorted([channel_type.value for channel_type in self.channel_types])
 
         if self.min_value is not None:
             ret["min_value"] = self.min_value
