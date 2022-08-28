74e06a5e4bc2351ed982c8eda7fcc452db2d08b7
i80287 <92759021+i80287@users.noreply.github.com>
2022-08-28 10:53:37 +0300

    Update localization.py

--- examples/application_commands/localization.py
+++ examples/application_commands/localization.py
@@ -24,11 +24,11 @@ bot = commands.Bot()
 )
 async def hello_command(interaction: Interaction):
 
-    if interaction.locale == Locale.de:
+    if interaction.locale == "de":
         await interaction.response.send_message("Hallo Welt!")
-    elif interaction.locale == Locale.fr:
+    elif interaction.locale == "fr":
         await interaction.response.send_message("Bonjour le monde!")
-    elif interaction.locale == Locale.es_ES:
+    elif interaction.locale == "es-ES":
         await interaction.response.send_message("¡Hola Mundo!")
     else:
         await interaction.response.send_message("Hello, world!")
