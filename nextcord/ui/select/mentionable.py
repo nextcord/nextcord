fe96b04dff3d537e5d5ed6ef5c5a156da3924a78
kingmigdor <kingmig.downtownofficialregion@gmail.com>
2022-10-15 20:38:50 +0200

    fix: change copyright in select files to be 2022

--- nextcord/ui/select/mentionable.py
+++ nextcord/ui/select/mentionable.py
@@ -1,7 +1,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2021-present tag-epic
+Copyright (c) 2022-present tag-epic
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
