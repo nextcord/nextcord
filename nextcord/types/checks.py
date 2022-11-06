bca6ff62062beb1c68c780b1a86aec9718fa7161
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-11-06 14:59:53 -0800

    fix: port nextcord/nextcord#888

--- nextcord/types/checks.py
+++ nextcord/types/checks.py
@@ -30,16 +30,16 @@ if TYPE_CHECKING:
 
     from ..interactions import Interaction
 
-T = TypeVar("T")
+    T = TypeVar("T")
 
-Coro = Coroutine[Any, Any, T]
-MaybeCoro = Union[T, Coro[T]]
-CoroFunc = Callable[..., Coro[Any]]
-ApplicationCheck = Union[
-    Callable[[Cog, Interaction], MaybeCoro[bool]], Callable[[Interaction], MaybeCoro[bool]]
-]
-ApplicationHook = Union[Callable[[Cog, Interaction], Coro[Any]], Callable[[Interaction], Coro[Any]]]
-ApplicationErrorCallback = Union[
-    Callable[[Cog, Interaction, Exception], Coro[Any]],
-    Callable[[Interaction, Exception], Coro[Any]],
-]
+    Coro = Coroutine[Any, Any, T]
+    MaybeCoro = Union[T, Coro[T]]
+    CoroFunc = Callable[..., Coro[Any]]
+    ApplicationCheck = Union[
+        Callable[[Cog, Interaction], MaybeCoro[bool]], Callable[[Interaction], MaybeCoro[bool]]
+    ]
+    ApplicationHook = Union[Callable[[Cog, Interaction], Coro[Any]], Callable[[Interaction], Coro[Any]]]
+    ApplicationErrorCallback = Union[
+        Callable[[Cog, Interaction, Exception], Coro[Any]],
+        Callable[[Interaction, Exception], Coro[Any]],
+    ]
