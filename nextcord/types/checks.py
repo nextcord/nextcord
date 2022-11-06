3861bd98f3cfbf75a5f0a077affe4c80aa70bb33
Jonah Lawrence <jonah@freshidea.com>
2022-11-06 09:18:44 -0700

    refactor: move to TYPE_CHECKING

--- nextcord/types/checks.py
+++ nextcord/types/checks.py
@@ -23,24 +23,25 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from typing import Any, Callable, Coroutine, TypeVar, Union
+from typing import TYPE_CHECKING, Any, Callable, Coroutine, TypeVar, Union
 
-from nextcord.application_command import ClientCog
+if TYPE_CHECKING:
+    from nextcord.application_command import ClientCog
 
-from ..interactions import Interaction
+    from ..interactions import Interaction
 
-T = TypeVar("T")
+    T = TypeVar("T")
 
-Coro = Coroutine[Any, Any, T]
-MaybeCoro = Union[T, Coro[T]]
-CoroFunc = Callable[..., Coro[Any]]
-ApplicationCheck = Union[
-    Callable[[ClientCog, Interaction], MaybeCoro[bool]], Callable[[Interaction], MaybeCoro[bool]]
-]
-ApplicationHook = Union[
-    Callable[[ClientCog, Interaction], Coro[Any]], Callable[[Interaction], Coro[Any]]
-]
-ApplicationErrorCallback = Union[
-    Callable[[ClientCog, Interaction, Exception], Coro[Any]],
-    Callable[[Interaction, Exception], Coro[Any]],
-]
+    Coro = Coroutine[Any, Any, T]
+    MaybeCoro = Union[T, Coro[T]]
+    CoroFunc = Callable[..., Coro[Any]]
+    ApplicationCheck = Union[
+        Callable[[ClientCog, Interaction], MaybeCoro[bool]], Callable[[Interaction], MaybeCoro[bool]]
+    ]
+    ApplicationHook = Union[
+        Callable[[ClientCog, Interaction], Coro[Any]], Callable[[Interaction], Coro[Any]]
+    ]
+    ApplicationErrorCallback = Union[
+        Callable[[ClientCog, Interaction, Exception], Coro[Any]],
+        Callable[[Interaction, Exception], Coro[Any]],
+    ]
