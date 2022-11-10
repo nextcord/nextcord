edb91a2eabb3ae60c477fe5e2b1956f2920bc78d
Sham <thesham@protonmail.com>
2022-11-10 21:52:32 +0200

    feat(flags): add Active Developer flag

--- nextcord/flags.py
+++ nextcord/flags.py
@@ -486,6 +486,14 @@ class PublicUserFlags(BaseFlags):
         """
         return UserFlags.known_spammer.value
 
+    @flag_value
+    def active_developer(self):
+        """:class:`bool`: Returns ``True`` if the user is a Known Spammer.
+
+        .. versionadded:: 2.0
+        """
+        return UserFlags.active_developer.value
+
     def all(self) -> List[UserFlags]:
         """List[:class:`UserFlags`]: Returns all public flags the user has."""
         return [public_flag for public_flag in UserFlags if self._has_flag(public_flag.value)]
