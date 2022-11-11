42a95f70431942b96060dd7b15bbf1fbca9149d5
ooliver1 <oliverwilkes2006@icloud.com>
2022-11-11 19:10:10 +0000

    fix: whoops

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ packages = [
     "nextcord.ext.application_checks",
     "nextcord.ext.commands",
     "nextcord.ext.tasks",
+    "nextcord.ui.select",
     # Compat
     "discord",
     "discord.types",
@@ -67,7 +68,6 @@ packages = [
     "discord.webhook",
     "discord.ext.commands",
     "discord.ext.tasks",
-    "discord.ui.select",
 ]
 
 setup(
