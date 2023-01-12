2fe887b061ad73c07bdb03165144d785f1d762e4
Gangsta-Muffin <austin.miles.castle@gmail.com>
2023-01-12 23:32:09 +0100

    refactor: Transfer View and Modal specific data and methods to their subclasses
    * Copy Interaction.edit() method to ViewInteraction
    * Copy Intenteraction.edit() to ModalSubmitInteraction
    * Remove Interaction.edit() from Interaction
    
    * Remove .message attribute from Interaction
    * Add .message attribute to ViewInteraction
    * Add .message attribute to ModalSubmitInteraction
    
    * Add attributes for ModalSubmit refined data (ModalSubmitInteraction)
    * add attributes for MessageComponent refined data (ViewInteraction)
    
    * Add comments for questioning changes

--- nextcord/interactions.py
+++ nextcord/interactions.py
@@ -191,16 +191,6 @@ class Interaction(Hashable, Generic[ClientT]):
         self.application_id: int = int(data["application_id"])
         self.locale: Optional[str] = data.get("locale")
         self.guild_locale: Optional[str] = data.get("guild_locale")
-
-        self.message: Optional[Message]
-        try:
-            message = data["message"]
-            self.message = self._state._get_message(int(message["id"])) or Message(
-                state=self._state, channel=self.channel, data=message  # type: ignore
-            )
-        except KeyError:
-            self.message = None
-
         self.user: Optional[Union[User, Member]] = None
         self._app_permissions: int = int(data.get("app_permissions", 0))
         self._permissions: int = 0
@@ -277,7 +267,7 @@ class Interaction(Hashable, Generic[ClientT]):
         return Permissions(self._permissions)
 
     @property
-    def app_permissions(self) -> Permissions:
+    def app_permissions(self) -> Permissions:  # Change method name to client_perms?
         """:class:`Permissions`: The resolved permissions of the bot in the channel, including overwrites.
 
         In a non-guild context where this doesn't apply, an empty permissions object is returned.
@@ -551,6 +541,96 @@ class Interaction(Hashable, Generic[ClientT]):
             allowed_mentions=allowed_mentions,
         )
 
+
+class SlashApplicationInteraction(Interaction):
+    def __init__(self, *, data: InteractionPayload, state: ConnectionState):
+        super().__init__(data=data, state=state)
+
+        self.application_command: Optional[
+            Union[SlashApplicationSubcommand, BaseApplicationCommand]
+        ] = None
+
+    def _from_data(self, data: InteractionPayload):
+        super()._from_data(data=data)
+
+        self.app_command_name: str = self.data["name"]  # type: ignore # self.data should be present here
+        self.app_command_id: int = self.data["id"]  # type: ignore # self.data should be present here
+
+        try:
+            self.options: list = self._get_application_options(self.data["options"])  # type: ignore # self.data should be present here
+        except KeyError:
+            self.options: list = []
+
+    def _set_application_command(
+        self, app_cmd: Union[SlashApplicationSubcommand, BaseApplicationCommand]
+    ):
+        self.application_command = app_cmd
+
+    def _get_application_options(self, data):
+        options_collection: list = []
+
+        for option in data:
+            options_collection.append(SlashOptionData(option))
+
+        return options_collection
+
+
+class ApplicationAutocompleteInteraction(Interaction):
+    def __init__(self, *, data: InteractionPayload, state: ConnectionState):
+        super().__init__(data=data, state=state)
+        self.application_command: Optional[
+            Union[SlashApplicationSubcommand, BaseApplicationCommand]
+        ] = None
+
+    def _from_data(self, data: InteractionPayload):
+        super()._from_data(data=data)
+
+        self.app_command_name: str = self.data["name"]  # type: ignore # self.data should be present here
+        self.app_command_id: int = self.data["id"]  # type: ignore # self.data should be present here
+
+        try:
+            self.options: list = self._get_application_options(self.data["options"])  # type: ignore # self.data should be present here
+        except KeyError:
+            self.options: list = []
+
+    def _set_application_command(
+        self, app_cmd: Union[SlashApplicationSubcommand, BaseApplicationCommand]
+    ):
+        self.application_command = app_cmd
+
+    def _get_application_options(self, data):
+        options_collection: list = []
+
+        for option in data:
+            if "focused" in option:
+                self.focused_option = SlashOptionData(option)
+                options_collection.append(self.focused_option)
+                continue
+
+            options_collection.append(SlashOptionData(option))
+
+        return options_collection
+
+
+class ViewInteraction(Interaction):
+    def __init__(self, *, data: InteractionPayload, state: ConnectionState):
+        super().__init__(data=data, state=state)
+
+    # Not necessary to call on init - super() calls it automatically
+    def _from_data(self, data: InteractionPayload):
+        super()._from_data(data=data)
+        
+        message = data["message"]  # type: ignore # should be present here - not sure why its causing issues
+        self.message = self._state._get_message(int(message["id"])) or Message(
+            state=self._state, channel=self.channel, data=message  # type: ignore
+        )
+        self.custom_id = self.data["custom_id"]  # type: ignore # self.data should be present here
+
+        try:
+            self.value = self.data["values"]  # type: ignore # self.data should be present here
+        except KeyError:
+            self.value = None
+
     async def edit(self, *args, **kwargs) -> Optional[Message]:
         """|coro|
 
@@ -593,84 +673,66 @@ class Interaction(Hashable, Generic[ClientT]):
         )
 
 
-class SlashApplicationInteraction(Interaction):
-    def __init__(self, *, data: InteractionPayload, state: ConnectionState):
-        super().__init__(data=data, state=state)
-
-        self.application_command: Optional[
-            Union[SlashApplicationSubcommand, BaseApplicationCommand]
-        ] = None
-
-    def _from_data(self, data: InteractionPayload):
-        super()._from_data(data=data)
-
-        self.app_command_name: str = self.data["name"]  # type: ignore # Data should be present here
-        self.app_command_id: int = self.data["id"]  # type: ignore # Data should be present here
-
-        try:
-            self.options: list = self._get_application_options(self.data["options"])  # type: ignore # Data should be present here
-        except KeyError:
-            self.options: list = []
-
-    def _set_application_command(
-        self, app_cmd: Union[SlashApplicationSubcommand, BaseApplicationCommand]
-    ):
-        self.application_command = app_cmd
-
-    def _get_application_options(self, data):
-        options_collection: list = []
-
-        for option in data:
-            options_collection.append(SlashOptionData(option))
-
-        return options_collection
-
-
-class ApplicationAutocompleteInteraction(Interaction):
-    def __init__(self, *, data: InteractionPayload, state: ConnectionState):
-        super().__init__(data=data, state=state)
-        self.application_command: Optional[
-            Union[SlashApplicationSubcommand, BaseApplicationCommand]
-        ] = None
-
-    def _from_data(self, data: InteractionPayload):
-        super()._from_data(data=data)
-
-        self.app_command_name: str = self.data["name"]  # type: ignore # Data should be present here
-        self.app_command_id: int = self.data["id"]  # type: ignore # Data should be present here
-
-        try:
-            self.options: list = self._get_application_options(self.data["options"])  # type: ignore # Data should be present here
-        except KeyError:
-            self.options: list = []
-
-    def _set_application_command(
-        self, app_cmd: Union[SlashApplicationSubcommand, BaseApplicationCommand]
-    ):
-        self.application_command = app_cmd
-
-    def _get_application_options(self, data):
-        options_collection: list = []
-
-        for option in data:
-            if "focused" in option:
-                self.focused_option = SlashOptionData(option)
-                options_collection.append(self.focused_option)
-                continue
-
-            options_collection.append(SlashOptionData(option))
-
-        return options_collection
-
-
-class ViewInteraction(Interaction):
-    def __init__(self, *, data: InteractionPayload, state: ConnectionState):
-        super().__init__(data=data, state=state)
-
-
 class ModalSubmitInteraction(Interaction):
     def __init__(self, *, data: InteractionPayload, state: ConnectionState):
         super().__init__(data=data, state=state)
+
+    def _from_data(self, data: InteractionPayload):
+        super()._from_data(data=data)
+
+        self.custom_id = self.data["custom_id"]  # type: ignore # self.data should be present here
+        self.components = self.data["components"]  # type: ignore # self.data should be present here
+
+        self.message: Optional[Message]
+        try:
+            message = data["message"]
+            print("found a message")
+            self.message = self._state._get_message(int(message["id"])) or Message(
+                state=self._state, channel=self.channel, data=message  # type: ignore
+            )
+        except KeyError:
+            self.message = None
+
+    async def edit(self, *args, **kwargs) -> Optional[Message]:
+        """|coro|
+
+        This is a shorthand function for helping in editing messages in
+        response to a component or modal submit interaction. If the
+        interaction has not been responded to, :meth:`InteractionResponse.edit_message`
+        is used. If the response :meth:`~InteractionResponse.is_done` then
+        the message is edited via the :attr:`Interaction.message` using
+        :meth:`Message.edit` instead.
+
+        Raises
+        ------
+        HTTPException
+            Editing the message failed.
+        InvalidArgument
+            You specified both ``embed`` and ``embeds`` or ``file`` and ``files``.
+        TypeError
+            An object not of type :class:`File` was passed to ``file`` or ``files``.
+        HTTPException
+            Editing the message failed.
+        InvalidArgument
+            :attr:`Interaction.message` was ``None``, this may occur in a :class:`Thread`
+            or when the interaction is not a component or modal submit interaction.
+
+        Returns
+        -------
+        Optional[:class:`Message`]
+            The edited message. If the interaction has not yet been responded to,
+            :meth:`InteractionResponse.edit_message` is used which returns
+            a :class:`Message` or ``None`` corresponding to :attr:`Interaction.message`.
+            Otherwise, the :class:`Message` is returned via :meth:`Message.edit`.
+        """
+        if not self.response.is_done():
+            return await self.response.edit_message(*args, **kwargs)
+        if self.message is not None:
+            return await self.message.edit(*args, **kwargs)
+        raise InvalidArgument(
+            "Interaction.message is None, this method can only be used in "
+            "response to a component or modal submit interaction."
+        )
 
 
 class InteractionResponse:
