2852357fa6e0be8a056a29318f3e158afbd4220c
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-01-22 19:18:26 +0100

    add missing MessageTypes

--- nextcord/types/message.py
+++ nextcord/types/message.py
@@ -73,7 +73,9 @@ class RoleSubscriptionData(TypedDict):
     is_renewal: bool
 
 
-MessageType = Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 18, 19, 20, 21]
+MessageType = Literal[
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 18, 19, 20, 21, 22, 23, 24, 25
+]
 
 
 class Message(TypedDict):
