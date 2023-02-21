0c6c69ceadb3397143b36607e78e82b4d16408a6
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-02-21 23:57:14 +0000

    refactor: remove mutable parameter defaults (#959)
    * refactor: remove mutable paramter defaults
    
    * Update nextcord/application_command.py
    
    ---------
    
    Co-authored-by: Ethan <47520067+Skelmis@users.noreply.github.com>

--- nextcord/application_command.py
+++ nextcord/application_command.py
@@ -3402,7 +3402,7 @@ def get_roles_from_interaction(state: ConnectionState, interaction: Interaction)
     return ret
 
 
-def unpack_annotated(given_annotation: Any, resolve_list: list[type] = []) -> type:
+def unpack_annotated(given_annotation: Any, resolve_list: Optional[list[type]] = None) -> type:
     """Takes an annotation. If the origin is Annotated, it will attempt to resolve it using the given list of accepted
     types, going from the last type and working up to the first. If no matches to the given list is found, the last
     type specified in the Annotated typehint will be returned.
@@ -3421,6 +3421,9 @@ def unpack_annotated(given_annotation: Any, resolve_list: list[type] = []) -> ty
     :class:`type`
         Resolved annotation.
     """
+    if resolve_list is None:
+        resolve_list = []
+
     # origin = typing.get_origin(given_annotation)  # TODO: Once Python 3.10 is standard, use this.
     origin = typing_extensions.get_origin(given_annotation)
     if origin is Annotated:
@@ -3441,7 +3444,7 @@ def unpack_annotated(given_annotation: Any, resolve_list: list[type] = []) -> ty
 
 
 def unpack_annotation(
-    given_annotation: Any, annotated_list: List[type] = []
+    given_annotation: Any, annotated_list: Optional[List[type]] = None
 ) -> Tuple[List[type], list]:
     """Unpacks the given parameter annotation into its components.
 
@@ -3459,6 +3462,9 @@ def unpack_annotation(
         and a list of unpacked literal arguments.
 
     """
+    if annotated_list is None:
+        annotated_list = []
+
     type_ret = []
     literal_ret = []
     # origin = typing.get_origin(given_annotation)  # TODO: Once Python 3.10 is standard, use this.
