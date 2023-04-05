5ed02d06386ba7b0ac009e9e8833c5f9f2cadb44
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-04-05 18:26:42 +0100

    chore: bump version to 2.4.2

--- nextcord/__init__.py
+++ nextcord/__init__.py
@@ -13,7 +13,7 @@ __title__ = "nextcord"
 __author__ = "Nextcord Developers & Rapptz"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015-2021 Rapptz & 2021-present Nextcord Developers"
-__version__ = "2.4.0"
+__version__ = "2.4.2"
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
