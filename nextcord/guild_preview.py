840bc3c905145f1c46e53a550404d2b2814c93f8
toolifelesstocode <luvuyosilubane50@gmail.com>
2023-04-18 10:51:19 +0200

    docs(GuildPreview): add versionadded

--- nextcord/guild_preview.py
+++ nextcord/guild_preview.py
@@ -34,6 +34,8 @@ class GuildPreview:
         All the emojis that the guild owns, if any.
     stickers: List[:class:`GuildSticker`]
         All the stickers that the guild owns, if any.
+
+    .. versionadded: 2.5
     """
 
     __slots__ = (
