fb708d03e5d47ac6c681d3bffb428da4f94b74bf
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-04-30 17:46:14 +0200

    feat(slash_utils): add rename decorator

--- nextcord/ext/slash_utils/slash.py
+++ nextcord/ext/slash_utils/slash.py
@@ -25,3 +25,40 @@ def describe(
         return DescribeWrapper(func)
 
     return wrapper
+
+
+def rename(
+    **kwargs: str,
+) -> Callable[
+    [Callable], Union[SlashApplicationCommand, SlashApplicationSubcommand, CallbackWrapper]
+]:
+    class RenameWrapper(CallbackWrapper):
+        def modify(self, app_cmd: SlashApplicationCommand):
+            option_names = {option.functional_name: option for option in app_cmd.options.values()}
+            for original_name, new_name in kwargs.items():
+                if option := option_names.get(original_name):
+                    # Remove the old name from the dict and add the new one
+                    # We must do this because the dict is used to call the callback
+                    option_names.pop(original_name)
+                    option.name = new_name
+                    option_names[new_name] = option
+                else:
+                    raise ValueError(
+                        f'{app_cmd.error_name} Could not find option "{original_name}" to rename.'
+                    )
+
+            # Update the options dict with the new name(s)
+            # We also need to sort it based on if the option is required or not -
+            # this is because required options must be listed first in the dict
+            app_cmd.options = {
+                name: option
+                for name, option in sorted(
+                    option_names.items(),
+                    key=lambda x: not x[1].required,
+                )
+            }
+
+    def wrapper(func):
+        return RenameWrapper(func)
+
+    return wrapper
