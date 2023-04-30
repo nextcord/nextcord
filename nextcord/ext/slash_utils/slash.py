08f556c093bf832449838d9b40a12ce94f2a88d9
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-04-30 18:24:26 +0200

    feat(slash_utils): add choices decorator

--- nextcord/ext/slash_utils/slash.py
+++ nextcord/ext/slash_utils/slash.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 
-from typing import Callable, Union
+from typing import Callable, Dict, Iterable, Union
 
 from nextcord import CallbackWrapper, SlashApplicationCommand, SlashApplicationSubcommand
 
@@ -23,5 +23,27 @@ def describe(
 
     def wrapper(func):
         return DescribeWrapper(func)
+
+    return wrapper
+
+
+def choices(
+    **kwargs: Union[Dict[str, Union[str, int, float]], Iterable[Union[str, int, float]]],
+) -> Callable[
+    [Callable], Union[SlashApplicationCommand, SlashApplicationSubcommand, CallbackWrapper]
+]:
+    class ChoicesWrapper(CallbackWrapper):
+        def modify(self, app_cmd: SlashApplicationCommand):
+            option_names = {option.functional_name: option for option in app_cmd.options.values()}
+            for given_name in kwargs:
+                if option := option_names.get(given_name):
+                    option.choices = kwargs[given_name]
+                else:
+                    raise ValueError(
+                        f'{app_cmd.error_name} Could not find option "{given_name}" to add choices to.'
+                    )
+
+    def wrapper(func):
+        return ChoicesWrapper(func)
 
     return wrapper
