5020b997b3fdec53d18894e555682e0f00cbd681
TrapDrap <69605288+TrapDrap@users.noreply.github.com>
2023-06-10 13:04:47 -0400

    TrapDrap nextcord prevent update (#2)
    * Readded updated to abc.py
    * Readded updated to view.py

--- nextcord/abc.py
+++ nextcord/abc.py
@@ -1554,7 +1554,7 @@ class Messageable:
             )
 
         ret = state.create_message(channel=channel, data=data)
-        if view:
+        if view and view.prevent_update:
             state.store_view(view, ret.id)
 
         if delete_after is not None:
