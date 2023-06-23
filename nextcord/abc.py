43a10856cfecbb518617ce377c98b8505dde4dce
spifory <luvuyosilubane50@gmail.com>
2023-06-23 13:15:10 +0200

    docs: fix markdown hyperlinks

--- nextcord/abc.py
+++ nextcord/abc.py
@@ -125,8 +125,8 @@ class User(Snowflake, Protocol):
         The user's discriminator.
 
         .. warning::
-          This field is deprecated, and will only return if the user has not yet migrated to the
-          new [username](https://dis.gd/usernames) update.
+            This field is deprecated, and will only return if the user has not yet migrated to the
+            new new `username <https://dis.gd/usernames>`_ update.
         .. deprecated:: 2.6
     avatar: :class:`~nextcord.Asset`
         The avatar asset the user has.
--- nextcord/user.py
+++ nextcord/user.py
@@ -314,7 +314,7 @@ class ClientUser(BaseUser):
 
         .. warning::
           This field is deprecated, and will only return if the user has not yet migrated to the
-          new [username](https://dis.gd/usernames) update.
+          new `username <https://dis.gd/usernames>`_ update.
         .. deprecated:: 2.6
     bot: :class:`bool`
         Specifies if the user is a bot account.
