6407f6f20e67a150204ef85548aa1db2146d43a6
Gangsta-Muffin <austin.miles.castle@gmail.com>
2023-08-22 00:26:27 +0200

    remove false copyright note in __init__

--- nextcord/interactions/__init__.py
+++ nextcord/interactions/__init__.py
@@ -4,7 +4,6 @@ nextcord.interactions
 
 Interactions collection for the Discord API
 
-:copyright: (c) 2015-2021 Rapptz
 :copyright: (c) 2021-present Nextcord Developers
 :license: MIT, see LICENSE for more details.
 """
