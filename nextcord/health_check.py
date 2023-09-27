e47f20fea954d33791906c4d69ed561d9eb0105a
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-09-27 12:12:23 +0200

    refactor(health_check): replace pkg_resources with importlib.metadata

--- nextcord/health_check.py
+++ nextcord/health_check.py
@@ -1,8 +1,7 @@
 # SPDX-License-Identifier: MIT
 
+from importlib.metadata import PackageNotFoundError, metadata
 from warnings import warn
-
-from pkg_resources import DistributionNotFound, get_distribution
 
 __all__ = ("incompatible_libraries",)
 
@@ -15,7 +14,7 @@ incompatible_libraries = ["discord.py", "discord", "pyfork", "enhanced-dpy", "py
 
 for library in incompatible_libraries:
     try:
-        get_distribution(library)
+        metadata(library)
         # Library is installed. Throw a warning
         message = (
             f"{library} is installed which is incompatible with nextcord. "
@@ -23,5 +22,5 @@ for library in incompatible_libraries:
         )
 
         warn(message, DistributionWarning, stacklevel=0)
-    except DistributionNotFound:
+    except PackageNotFoundError:
         pass
