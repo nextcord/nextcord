7e857e5cf75659f6bcaa61ea1f89fd70112e709a
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2023-09-27 20:48:22 -0700

    docs: rewrite to be less dynamic

--- nextcord/ui/modal.py
+++ nextcord/ui/modal.py
@@ -173,8 +173,7 @@ class Modal:
 
             .. note::
 
-                At the current time, Discord supports only Text Input items in Modals.
-                The current Discord Docs page for supported items is [here](https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-response-object-modal).
+                The only items that are currently supported are :class:`TextInput`\s.
 
         Raises
         ------
