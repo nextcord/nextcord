1673256fce27483ceec32d522bd3241af4458231
kingmigdor <kingmig.downtownofficialregion@gmail.com>
2023-10-22 07:38:37 +0200

    feat(entitlements): adding something else that looks important

--- nextcord/entitlement.py
+++ nextcord/entitlement.py
@@ -1,11 +1,15 @@
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
 from datetime import datetime
-from types.entitlement import SKU as SKUPayload, Entitlement as EntitlementPayload
 from typing import Optional
 
 from enums import EntitlementType, SKUType
 from flags import SKUFlags
 from object import Object
 from utils import parse_time
+from types.entitlement import SKU as SKUPayload, Entitlement as EntitlementPayload
 
 
 class Entitlement(Object):
