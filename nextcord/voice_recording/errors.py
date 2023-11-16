9122ebd43e91878d8654bf37cd5416a4c83209ec
TrapDrap <liulouis1080@gmail.com>
2023-11-16 07:57:24 -0500

    refine error hierarchy
    Changes to be committed:
    	modified:   nextcord/voice_recording/errors.py

--- nextcord/voice_recording/errors.py
+++ nextcord/voice_recording/errors.py
@@ -3,49 +3,63 @@
 import nextcord.errors as nc_errors
 
 
-class OngoingRecordingError(nc_errors.DiscordException):
+class RecordingException(nc_errors.DiscordException):
+    """Raised when any recording error occurs.
+
+    .. versionadded:: 3.0
     """
-    Raised when an forbidden action is performed when a recording is ongoing.
+
+class OngoingRecordingError(RecordingException):
+    """Raised when an forbidden action is performed when a recording is ongoing.
+
+    Subclass of :exc:`RecordingException`
     """
 
 
-class NotRecordingError(nc_errors.DiscordException):
-    """
-    Raised when stopping a recording is attempted while a recording is not ongoing.
+class NotRecordingError(RecordingException):
+    """Raised when stopping a recording is attempted while a recording is not ongoing.
+
+    Subclass of :exc:`RecordingException`
     """
 
 
-class NotConnectedError(nc_errors.DiscordException):
-    """
-    Raised when attempting to record without being in a voice channel.
+class NotConnectedError(RecordingException):
+    """Raised when attempting to record without being in a voice channel.
+
+    Subclass of :exc:`RecordingException`
     """
 
 
-class TmpNotFound(nc_errors.DiscordException):
-    """
-    Raised when failed attempting to access temp files.
+class TmpNotFound(RecordingException):
+    """Raised when failed attempting to access temp files.
+
+    Subclass of :exc:`RecordingException`
     """
 
 
-class EmptyRecordingError(nc_errors.DiscordException):
-    """
-    Raised when the recording was empty.
+class EmptyRecordingError(RecordingException):
+    """Raised when the recording was empty.
+
+    Subclass of :exc:`RecordingException`
     """
 
 
-class NoFFmpeg(nc_errors.DiscordException):
-    """
-    Raised if ffmpeg was not found.
+class NoFFmpeg(RecordingException):
+    """Raised if ffmpeg was not found.
+
+    Subclass of :exc:`RecordingException`
     """
 
 
-class MultipleHandlersError(nc_errors.DiscordException):
-    """
-    Raised when trying to set multiple audio handlers.
+class MultipleHandlersError(RecordingException):
+    """Raised when trying to set multiple audio handlers.
+    
+    Subclass of :exc:`RecordingException`
     """
 
 
-class ExportUnavailable(nc_errors.DiscordException):
-    """
-    Raised when exporting is not possible due to an audio handler having been set.
+class ExportUnavailable(RecordingException):
+    """Raised when exporting is not possible due to an audio handler having been set.
+
+    Subclass of :exc:`RecordingException`
     """
