e0b7f9f183dc9fc9c4f2f898c9d30ed0c3a12a92
acco4321 <46668359+acco4321@users.noreply.github.com>
2024-06-14 17:35:57 +1000

    feat: add list of webhooks to forum, stage, and voice channels (#1177)
    * feat: add list of webhooks to forum, stage, and voice channels
    
    * fix docstring (version)
    
    * style: auto fixes from pre-commit hooks
    
    * Update version per review
    
    Co-authored-by: Emma Terzioglu <50607143+EmreTech@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Acco <contact@acco4321.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Emma Terzioglu <50607143+EmreTech@users.noreply.github.com>

--- nextcord/channel.py
+++ nextcord/channel.py
@@ -1474,6 +1474,31 @@ class ForumChannel(abc.GuildChannel, Hashable):
         )
         return Webhook.from_state(data, state=self._state)
 
+    async def webhooks(self) -> List[Webhook]:
+        """|coro|
+
+        Gets the list of webhooks from this channel.
+
+        Requires :attr:`~.Permissions.manage_webhooks` permissions.
+
+        .. versionadded:: 3.0
+
+        Raises
+        ------
+        Forbidden
+            You don't have permissions to get the webhooks.
+
+        Returns
+        -------
+        List[:class:`Webhook`]
+            The webhooks for this channel.
+        """
+
+        from .webhook import Webhook
+
+        data = await self._state.http.channel_webhooks(self.id)
+        return [Webhook.from_state(d, state=self._state) for d in data]
+
 
 class VocalGuildChannel(abc.Connectable, abc.GuildChannel, Hashable):
     __slots__ = (
@@ -2014,6 +2039,31 @@ class VoiceChannel(VocalGuildChannel, abc.Messageable):
         )
         return Webhook.from_state(data, state=self._state)
 
+    async def webhooks(self) -> List[Webhook]:
+        """|coro|
+
+        Gets the list of webhooks from this channel.
+
+        Requires :attr:`~.Permissions.manage_webhooks` permissions.
+
+        .. versionadded:: 3.0
+
+        Raises
+        ------
+        Forbidden
+            You don't have permissions to get the webhooks.
+
+        Returns
+        -------
+        List[:class:`Webhook`]
+            The webhooks for this channel.
+        """
+
+        from .webhook import Webhook
+
+        data = await self._state.http.channel_webhooks(self.id)
+        return [Webhook.from_state(d, state=self._state) for d in data]
+
 
 class StageChannel(VocalGuildChannel, abc.Messageable):
     """Represents a Discord guild stage channel.
@@ -2329,6 +2379,31 @@ class StageChannel(VocalGuildChannel, abc.Messageable):
             # the payload will always be the proper channel payload
             return self.__class__(state=self._state, guild=self.guild, data=payload)  # type: ignore
         return None
+
+    async def webhooks(self) -> List[Webhook]:
+        """|coro|
+
+        Gets the list of webhooks from this channel.
+
+        Requires :attr:`~.Permissions.manage_webhooks` permissions.
+
+        .. versionadded:: 3.0
+
+        Raises
+        ------
+        Forbidden
+            You don't have permissions to get the webhooks.
+
+        Returns
+        -------
+        List[:class:`Webhook`]
+            The webhooks for this channel.
+        """
+
+        from .webhook import Webhook
+
+        data = await self._state.http.channel_webhooks(self.id)
+        return [Webhook.from_state(d, state=self._state) for d in data]
 
 
 class CategoryChannel(abc.GuildChannel, Hashable):
