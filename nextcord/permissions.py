45f698043b31264330913497c15ed3e4c7a539b9
xXenvy <quentisxd@gmail.com>
2024-06-27 05:54:27 +0200

    feat: update permissions.py

--- nextcord/permissions.py
+++ nextcord/permissions.py
@@ -154,8 +154,11 @@ class Permissions(BaseFlags):
            Added :attr:`create_public_threads`, :attr:`create_private_threads`, :attr:`manage_threads`,
            :attr:`use_external_stickers`, :attr:`send_messages_in_threads` and
            :attr:`request_to_speak` permissions.
+
+        .. versionchanged:: 3.0
+           Added :attr:`set_voice_channel_status` permission.
         """
-        return cls(0b111110110110011111101111111111101010001)
+        return cls(0b1000000001000000000000011111100000000001100000000)
 
     @classmethod
     def general(cls) -> Self:
