9083912f1fa5e62778d4b387753bbd91bc45aef8
Emma Terzioglu <emreterzioglu49@gmail.com>
2024-12-24 13:48:37 -0400

    refactor!: rewrite async iterators (#950)
    * refactor!: rewrite async iterators to not use classes
    
    * refactor!: rewrite ban & audit log iterators
    
    * docs: represent changes in Reaction.users docstring
    
    * refactor!: finish iterator rewrite
    
    * style: auto fixes from pre-commit hooks
    
    * refactor: update audit log iterators
    
    * style: auto fixes from pre-commit hooks
    
    * docs: add python doc references for async iterators
    
    * style: auto fixes from pre-commit hooks
    
    * typing: fix pyright errors
    
    * style: auto fixes from pre-commit hooks
    
    * fix(Guild.audit_logs): value of enum was passed into iterator
    
    * refactor(utils)!: remove chunk in favor of as_chunks
    
    * docs(iterators.py): add versionchanged
    
    * refactor: rewrite iterators to reflect original code
    
    * style: auto fixes from pre-commit hooks
    
    * feat: port nextcord/nextcord#1093
    
    * style: auto fixes from pre-commit hooks
    
    * style: auto fixes from pre-commit hooks
    
    * style: resolve ruff errors
    
    * style: auto fixes from pre-commit hooks
    
    * refactor(utils)!: remove anext helper
    nextcord maintainers have officially stated that the next version will have a 3.12 version minimum, which contains the builtin anext function.
    
    * docs: forgot asynciter tag
    
    * refactor: code smell, switch back to `reversed` for reversing data
    in an earlier state of this pr, I switched any iterator that used  the reversed tool to sorting the list with the id as a key. however, from my own tests, this strategy does not work as expected. therefore, I have reverted it.
    
    * style: auto fixes from pre-commit hooks
    
    * docs: remove obsolete AsyncIterator class
    
    * refactor(history iterator): remove obsolete 101 limit check
    
    ---------
    
    Co-authored-by: EmreTech <50607143+EmreTech@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Oliver Wilkes <oliverwilkes2006@icloud.com>

--- nextcord/client.py
+++ nextcord/client.py
@@ -11,6 +11,7 @@ import traceback
 from typing import (
     TYPE_CHECKING,
     Any,
+    AsyncIterator,
     Callable,
     Coroutine,
     Dict,
@@ -57,7 +58,7 @@ from .guild_preview import GuildPreview
 from .http import HTTPClient
 from .interactions import Interaction
 from .invite import Invite
-from .iterators import GuildIterator
+from .iterators import guild_iterator
 from .mentions import AllowedMentions
 from .object import Object
 from .stage_instance import StageInstance
@@ -1466,8 +1467,10 @@ class Client:
         with_counts: bool = False,
         before: Optional[SnowflakeTime] = None,
         after: Optional[SnowflakeTime] = None,
-    ) -> GuildIterator:
-        """Retrieves an :class:`.AsyncIterator` that enables receiving your guilds.
+    ) -> AsyncIterator[Guild]:
+        """|asynciter|
+
+        Returns an async iterator that enables receiving your guilds.
 
         .. note::
 
@@ -1485,11 +1488,6 @@ class Client:
 
             async for guild in client.fetch_guilds(limit=150):
                 print(guild.name)
-
-        Flattening into a list ::
-
-            guilds = await client.fetch_guilds(limit=150).flatten()
-            # guilds is now a list of Guild...
 
         All parameters are optional.
 
@@ -1527,7 +1525,9 @@ class Client:
         :class:`.Guild`
             The guild with the guild data parsed.
         """
-        return GuildIterator(self, limit=limit, before=before, after=after, with_counts=with_counts)
+        return guild_iterator(
+            self, limit=limit, before=before, after=after, with_counts=with_counts
+        )
 
     async def fetch_template(self, code: Union[Template, str]) -> Template:
         """|coro|
