aa3a895bf50f483fe2ee9c2864f8f507449b1f96
sdmway <vladyslav.p.e@gmail.com>
2025-02-17 18:28:06 +0100

    typing: add incidents_data

--- nextcord/types/guild.py
+++ nextcord/types/guild.py
@@ -67,6 +67,13 @@ GuildFeature = Literal[
 ]
 
 
+class IncidentsData(TypedDict):
+    invites_disabled_until: Optional[str]
+    dms_disabled_until: Optional[str]
+    dm_spam_detected_at: NotRequired[Optional[str]]
+    raid_detected_at: NotRequired[Optional[str]]
+
+
 class _BaseGuildPreview(UnavailableGuild):
     name: str
     icon: Optional[str]
@@ -128,6 +135,7 @@ class Guild(_BaseGuildPreview):
     stickers: NotRequired[List[GuildSticker]]
     premium_progress_bar_enabled: bool
     safety_alerts_channel_id: Optional[Snowflake]
+    incidents_data: Optional[IncidentsData]
 
 
 class InviteGuild(Guild, total=False):
