ba44660ec7e9f07228a42bff7d9bd5c48b734a25
teaishealthy <teaishealthy@protonmail.com>
2025-10-27 17:16:57 +0100

    fix(http): add proxy to HTTPClient.recreate (#1257)

--- nextcord/http.py
+++ nextcord/http.py
@@ -647,6 +647,8 @@ class HTTPClient:
             self.__session = aiohttp.ClientSession(
                 connector=self._connector,
                 ws_response_class=DiscordClientWebSocketResponse,
+                proxy=self._proxy,
+                proxy_auth=self._proxy_auth,
             )
             await self._start_ratelimit_shedding_loop()
 
